1c6c4e3a2affc9f0b2ffa06ea6a22ec4eaa7efa4
Hemanth Krishna <hkpdev008@gmail.com>
2021-03-04 22:04:00 +0530

    Initial commit

---
+++ app/src/main/java/com/benrostudios/c2c2021/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.benrostudios.c2c2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
