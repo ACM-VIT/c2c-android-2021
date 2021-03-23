4997502a712c77d3c991193cf029f5a2169a0a51
Dinesh B S <dineshbs444@gmail.com>
2021-03-24 00:20:53 +0530

    Added 3 on-boarding screens!

--- app/src/main/java/com/benrostudios/c2c2021/SplashActivity.kt
+++ app/src/main/java/com/benrostudios/c2c2021/SplashActivity.kt
@@ -20,7 +20,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         Handler().postDelayed(
             {
-                startActivity(Intent(this, MainActivity::class.java))
+                startActivity(Intent(this, WelcomeActivity::class.java))
                 finish()
             }, SPLASH_TIME_OUT
         )
