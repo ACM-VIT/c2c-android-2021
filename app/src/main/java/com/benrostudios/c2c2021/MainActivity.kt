adadbfeba1ce73df3b42329f6f994b3de298ec3d
Dinesh B S <dineshbs444@gmail.com>
2021-03-23 20:13:14 +0530

    Set up Bottom nav!

--- app/src/main/java/com/benrostudios/c2c2021/MainActivity.kt
+++ app/src/main/java/com/benrostudios/c2c2021/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.benrostudios.c2c2021
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    var navController: NavController? = null
+    var bottomNavigationView: BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?
+        if (navHostFragment != null) {
+            navController = navHostFragment.navController
+        }
+        NavigationUI.setupWithNavController(bottomNavigationView!!, navController!!)
+
     }
 }
